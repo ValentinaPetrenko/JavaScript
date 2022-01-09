37ac607fb594f371b18ef8d588647bca9da1703a
Valentina Petrenko <valushka0490@gmail.com>
2022-01-09 14:46:31 +0200

    add Lesson1HW.js

---
+++ Lesson1HW.js
@@ -0,0 +1,53 @@
+console.log("Task 2")
+console.log (typeof NaN)
+console.log (typeof Infinity)
+console.log (typeof 5)
+console.log (typeof "Hello")
+console.log (typeof true)
+console.log (typeof false)
+console.log (typeof null)
+console.log (typeof a)
+console.log (typeof {name:"Valentina"})
+
+console.log("Task 3")
+let Name
+if (Name=String) {
+    alert("Привет, Валентина!");
+} else {
+    alert("Ошибка, не тот тип данных");
+}
+
+console.log ("Task 4")
+
+let username=Name=String?"Привет, Валентина!":"Ошибка, не тот тип данных";
+
+
+console.log ("Task 5")
+let fDayOff=6;
+switch (fDayOff) {
+    case 1:
+        console.log("working day");
+        break;
+    case 2:
+        console.log("working day");
+        break;
+    case 3:
+        console.log("working day");
+        break;
+    case 4:
+        console.log("working day");
+        break;
+    case 5:
+        console.log("working day");
+        break;
+    case 6:
+        console.log("weekend");
+        break;
+    case 7:
+        console.log("weekend");
+        break;
+    default:
+        console.log("There is no such day of the week");
+       
+}
+
